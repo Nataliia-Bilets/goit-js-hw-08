50741a2ec0fa366c790b2e23e03a3e07a303766b
Nataliia-Bilets <natalia_bilets@ukr.net>
2022-12-22 19:56:36 +0200

    hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = galleryCardsMarkup();
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function galleryCardsMarkup() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+     <img class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`}).join('');
+}
+let galleryCard = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    close: true,
+});
+
 console.log(galleryItems);
