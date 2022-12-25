782b828c2208b0eff340118c99fca44101ca3a71
Nataliia-Bilets <natalia_bilets@ukr.net>
2022-12-26 01:35:22 +0200

    update hw-08-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,34 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
+dataFromLocalStorage();
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
+function onFormData() {
+    const formData = {email: email.value, message: message.value};
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.preventDefault();
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
   e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 };
-dataFromLocalStorage();
 
 function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
+    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(data);
   if (data) {
     email.value = data.email;
     message.value = data.message;
   }
 };
+
 
